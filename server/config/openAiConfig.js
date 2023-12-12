cf422c90f187486220d350c3c0548cfa42f1ff04
Doron-Breska <124248816+Doron-Breska@users.noreply.github.com>
2023-12-13 00:46:06 +0100

    new open AI model

--- server/config/openAiConfig.js
+++ server/config/openAiConfig.js
@@ -1,30 +1,24 @@
 import { Configuration, OpenAIApi } from "openai";
 
-
-const aiRole = 
-`Your role is to be a succulents/cacteen specialist.
+const aiRole = `Your role is to be a succulents/cacteen specialist.
 
 Each time I will provide you one species of succulents/cacteen.
 You need to send me back a guidance/tips for how to grow and maintain this species of plant (you can also add some info about this species).
 
 I want to get the response in one string of text.
 Please start each response with "For this species of Succulent...."
-}`
-
-
+}`;
 
 const openAiConfig = async (speciesName) => {
-
- let prompt = aiRole.replace(/SPECIES_NAME/g, speciesName);
+  let prompt = aiRole.replace(/SPECIES_NAME/g, speciesName);
 
   const configuration = new Configuration({
-      apiKey: process.env.OPENAI_API_KEY,
-      
+    apiKey: process.env.OPENAI_API_KEY,
   });
   const openai = new OpenAIApi(configuration);
- 
+
   const response = await openai.createCompletion({
-    model: "text-davinci-003",
+    model: "gpt-3.5-turbo-instruct",
     prompt: prompt,
     temperature: 0.7,
     max_tokens: 2000,
@@ -32,15 +26,8 @@ const openAiConfig = async (speciesName) => {
     frequency_penalty: 0,
     presence_penalty: 0,
   });
-  console.log("respond from open ai" , response.data);
+  console.log("respond from open ai", response.data);
   return response.data;
 };
 
-
 export default openAiConfig;
-
-
-
-
-
-
